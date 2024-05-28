15e8836549a279f036ace3427f795fc79f9cc204
nebuola <busraberfin24@gmail.com>
2024-05-28 20:38:25 +0300

    Initial commit

---
+++ app/src/main/java/com/beecoding/myapplication/MainActivity.kt
@@ -0,0 +1,143 @@
+package com.beecoding.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.beecoding.myapplication.ui.theme.LemonadeTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LemonadeTheme {
+                Surface {
+                    LemonadeApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonadeApp(modifier: Modifier = Modifier) {
+
+    var currentStep by remember { mutableIntStateOf(1) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        when (currentStep) {
+            1 -> {
+                LemonadeButton(
+                    imageResource = R.drawable.lemon_tree,
+                    textResource = R.string.tap_the_lemon_tree_to_pick_a_lemon,
+                    contentDesc = R.string.lemon_tree,
+                    callback = { currentStep = 2 }
+                )
+            }
+
+            2 -> {
+                LemonadeButton(
+                    imageResource = R.drawable.lemon_squeeze,
+                    textResource = R.string.keep_tapping_the_lemon_to_squeeze_it,
+                    contentDesc = R.string.lemon,
+                    callback = { currentStep = 3 }
+                )
+            }
+
+            3 -> {
+                LemonadeButton(
+                    imageResource = R.drawable.lemon_drink,
+                    textResource = R.string.tap_the_lemonade_to_drink_it,
+                    contentDesc = R.string.glass_of_lemonade,
+                    callback = { currentStep = 4 }
+                )
+            }
+
+            4 -> {
+                LemonadeButton(
+                    imageResource = R.drawable.lemon_restart,
+                    textResource = R.string.tap_the_empty_glass_to_start_again,
+                    contentDesc = R.string.empty_glass,
+                    callback = { currentStep = 1 }
+                )
+            }
+
+            else -> {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonadeButton(
+    modifier: Modifier = Modifier,
+    imageResource: Int,
+    textResource: Int,
+    contentDesc: Int,
+    callback: () -> Unit
+) {
+    Button(
+        onClick = callback,
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(203, 235, 212)
+        ),
+        shape = RoundedCornerShape(20)
+    ) {
+        Image(
+            painterResource(id = imageResource),
+            contentDescription = stringResource(id = contentDesc)
+        )
+    }
+
+    Spacer(modifier = Modifier.padding(16.dp))
+
+    Text(
+        text = stringResource(id = textResource),
+        fontSize = 18.sp
+    )
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun GreetingPreview() {
+    LemonadeTheme {
+        LemonadeApp()
+    }
+}
